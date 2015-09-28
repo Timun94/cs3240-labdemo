e7de8e92d1315e488cccfbf245c4194693bd273f
Timun94 <mms4ba@virginia.edu>
2015-09-28 17:54:47 -0400

    .

---
+++ new2.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Matt
+#
+# Created:     28/09/2015
+# Copyright:   (c) Matt 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print("This does nothing...")
+
+if __name__ == '__main__':
+    main()
