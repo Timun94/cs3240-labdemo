0fb6a2700e4250d289703320c43d134d02ff8c3a
Timun94 <mms4ba@virginia.edu>
2015-09-28 17:20:52 -0400

    message function helper

---
+++ helper.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Matt
+#
+# Created:     28/09/2015
+# Copyright:   (c) Matt 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+def greeting(msg):
+    print(msg)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
